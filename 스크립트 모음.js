da3994d1bfcc84394ec3dcc3a27af3c681e84af7
EunhyeHe <grace9695@naver.com>
2026-07-17 16:11:53 +0900

    파일추가2

---
+++ 스크립트 모음.js
@@ -0,0 +1,60 @@
+// 연습용
+
+document.addEventListener("DOMContentLoaded", function () {
+    let 
+    
+});
+
+document.querySelectorAll('.class-name').forEach() {
+
+}
+
+
+// GSAP
+// GSAP ScrollTrigger로 이미지 전환 제어
+gsap.registerPlugin(ScrollTrigger);
+gsap.to({}, {
+    scrollTrigger: {
+        trigger: '.about-intro',
+        start: 'top center',
+        end: '10%',
+        scrub: true,
+        markers : false,
+        onUpdate: (self) => {
+            const progress = self.progress * images.length;
+            const newIndex = Math.floor(progress); // 현재 인덱스 계산
+            if (newIndex !== currentIndex) {
+                changeImage();
+            }
+        }
+    }
+});
+
+// 텍스트 슬라이드
+const txtWordPs = document.querySelectorAll('.txt-word p');
+txtWordPs.forEach((p, index) => {
+    gsap.fromTo(p,
+        { opacity: 0, y: 20 },
+        {
+            opacity: 1, y: 0,  // 스크롤하면서 opacity가 1로, y축 이동
+            duration: 1,  // 전환 시간
+            scrollTrigger: {
+                trigger: p,
+                start: 'top 60%',  // 화면에서 80%에 도달하면 나타나기 시작
+                end: 'top 50%',    // 화면의 50%에 도달하면 애니메이션 끝
+                scrub: true,
+                markers: false,
+                toggleActions: 'play none none reverse'  // 애니메이션의 동작 설정
+            }
+        });
+});
+
+// career-imgShowup
+let careerItems = document.querySelectorAll('.about-career .career-list p');
+
+careerItems.forEach(item => {
+    let img = item.querySelector('span img');
+    item.addEventListener('mouseover', function() {
+        img.classList.add('visible');
+    });
+});
